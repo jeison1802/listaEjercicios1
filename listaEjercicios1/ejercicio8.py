0f082fd733bb3948d26b33331144a194fa64a050
jeison1802 <126401829+jeison1802@users.noreply.github.com>
2023-03-03 23:47:12 -0500

    Add files via upload

---
+++ listaEjercicios1/ejercicio8.py
@@ -0,0 +1,15 @@
+
+### 8. Escribir un programa que almacene la cadena de caracteres *contraseña* en una variable, pregunte el usuario 
+#      por la contraseña e imprima por pantalla si la contraseña introducida por el usuario coincide con la guardada 
+#      en la variable sin tener en cuenta mayúsculas y minúsculas. 
+
+cont = "contraseña"  
+
+c = input("Ingrese la contraseña: ")
+c_lower = c.lower()
+
+if (cont == c_lower):
+    print("Coincide con la variable guardada")
+else: 
+    print("No coincide con la variable guardada")
+    
